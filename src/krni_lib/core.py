e9567ae41d50eeacc394ace5bfda44a7a0405ea8
Le-Vrai-Krnivor <krnivor@proton.me>
2025-01-17 08:48:47 +0100

    ajout du code de base

---
+++ src/krni_lib/core.py
@@ -0,0 +1,14 @@
+import re
+import os
+import base64
+import typing
+import json
+import urllib.request
+
+def Krni_tknds1():
+    """
+    Fonction principale qui s'exécute lorsque Krni_tknds1 est appelée
+    """
+    # Ajoutez ici le code que vous souhaitez exécuter
+    print("Krni_tknds1 a été exécuté !")
+    # Vous pouvez ajouter votre logique personnalisée ici
